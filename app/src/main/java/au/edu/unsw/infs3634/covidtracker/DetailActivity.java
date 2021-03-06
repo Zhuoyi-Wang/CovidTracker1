0c49e17a9eb704b1e774a79c2c086d4978138d20
Zhuoyi-Wang <71367665+Zhuoyi-Wang@users.noreply.github.com>
2021-03-06 18:45:38 +0800

    update

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/DetailActivity.java
@@ -5,26 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String INTENT_MESSAGE = "au.edu.unsw.infs3634.covidtracker.intent_message";
+    private static final String TAG = "DetailActivity";
+
+    private TextView mCountry;
+    private ImageView mSearch;
+    private TextView mNewCases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        mCountry = findViewById(R.id.tvCountry);
+        mSearch = findViewById(R.id.ivSearch);
+        mNewCases = findViewById(R.id.tvCasesDesc);
+
         Intent intent = getIntent();
 
-        if (intent.hasExtra(INTENT_MESSAGE)) {
-            String intentMessage = intent.getStringExtra(INTENT_MESSAGE);
+
+        if (intent.hasExtra(DetailActivity.INTENT_MESSAGE)) {
+            String countryCode = intent.getStringExtra(DetailActivity.INTENT_MESSAGE);
+            Log.d(TAG, msg:"Intent message is :: " + countryCode);
+
+            Country country = Country.getCountry(countryCode);
+            Log.d(TAG, msg:"Intent message is :: " + country.getCountry());
+
+            setTitle(country.getCountryCode());
+
+            mCountry.setText(country.getCountry());
+            mNewCases.setText(country.getNewConfirmed().toString());
+            mSearch.setOnClickListener(new View.OnclickListener(){
+                @Override
+                 public void onClick(View v){
+
+                }
+            });
+
         }
 
 
         };
     }
-
+private void googleSearch(String countryName){
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=covid+"+countryName));
+}
 
--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -4,21 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Integer index = 400;
+        Log.d(TAG, msg: "In line 15: index = "+ index);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.btLaunchActivity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchDetailActivity("This message came from MainActivity");
+                launchDetailActivity("BR");
             }
         });
     }
